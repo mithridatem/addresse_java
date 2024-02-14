113aa555a2365f5a6d13f347d428cbc75a2821f6
mithridatem <mathieumithridate@adrar-formation.com>
2024-02-14 10:38:54 +0100

    Refactor Request

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import com.addresse.model.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         //instancier un objet User
         User test = new User("Mithridate","Mathieu","test@test.com","123456");
-
-        System.out.println(UserManager.addUser(test).getNom());
+        //tester si l'enregistrement existe
+        //UserManager.getField(test);
+        System.out.println(UserManager.findUser2(test).getNom());
+       /* if(UserManager.findUser(test).getNom()!=null){
+            System.out.println("Le compte existe déja");
+        }
+        else {
+            System.out.println("Le compte : " + UserManager.addUser(test).getNom() + " a été ajouté en BDD");
+        }*/
     }
 }
--- src/com/addresse/model/UserManager.java
+++ src/com/addresse/model/UserManager.java
@@ -1,8 +1,7 @@
 package com.addresse.model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.lang.reflect.Field;
+import java.sql.*;
 
 public class UserManager {
     private static Connection connexion = DbConnexion.getConnexion();
@@ -39,5 +38,65 @@ public class UserManager {
             e.printStackTrace();
         }
         return useradd;
+    }
+    public static User findUser(User user){
+        User verif = new User();
+        try{
+            //connexion
+            Statement smt = connexion.createStatement();
+            //requête
+            String req = "SELECT id, nom, prenom,email, pwd FROM users WHERE email = ?";
+            //préparer la requête
+            PreparedStatement preparedStatement = connexion.prepareStatement(req);
+            //bind paramètre email
+            test(user, verif, preparedStatement);
+            smt.close();
+            connexion.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return verif;
+    }
+    public static User findUser2(User user){
+        User verif = new User();
+        try {
+            String req = "SELECT id, nom, prenom, email, pwd FROM users WHERE email = ?";
+            PreparedStatement preparedStatement = request(user,req);
+            test(user, verif, preparedStatement);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return verif;
+    }
+
+    private static void test(User user, User verif, PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement.setString(1, user.getEmail());
+        ResultSet rs = preparedStatement.executeQuery();
+        while(rs.next()){
+            //test si la requête retourne un résultat
+            if(rs.getString(1)!= null){
+                //setter les valeurs dans l'objet User de return
+                verif.setId(rs.getInt("id"));
+                verif.setNom(rs.getString("nom"));
+                verif.setPrenom(rs.getString("prenom"));
+                verif.setEmail(rs.getString("email"));
+                verif.setPassword(rs.getString("pwd"));
+            }
+        }
+    }
+
+    private static PreparedStatement request(User user, String req) throws SQLException, IllegalAccessException {
+        //connexion
+        Statement smt = connexion.createStatement();
+        //préparer la requête
+        return connexion.prepareStatement(req);
+    }
+    public static void getField(User user) throws IllegalAccessException {
+        for (Field field : user.getClass().getDeclaredFields()) {
+            field.setAccessible(true);//Obligatoire si le field est private
+            System.out.println(field.get(user));
+        }
     }
 }
